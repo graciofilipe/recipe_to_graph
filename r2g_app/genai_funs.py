bebbb82c97a2de1e5c52f5613b7b5eb295bfa5c2
graciofilipe <gracio.contact@gmail.com>
2025-05-07 06:35:57 +0000

    revert model to old option.

--- r2g_app/genai_funs.py
+++ r2g_app/genai_funs.py
@@ -6,7 +6,7 @@ from google.genai import types
 
 DEFAULT_VERTEX_PROJECT_ID = os.getenv("PROJECT_ID")
 DEFAULT_VERTEX_LOCATION = "us-central1"
-DEFAULT_MODEL_NAME = "gemini-2.5-pro-preview-05-06"
+DEFAULT_MODEL_NAME = "gemini-2.5-pro-exp-03-25" # "#"gemini-2.5-pro-preview-05-06"
 DEFAULT_TEMPERATURE = 0.2
 DEFAULT_TOP_P = 1.0
 DEFAULT_MAX_TOKENS = 8048
