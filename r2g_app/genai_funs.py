8cecaad567f722f5d7a284a83aebafa8017acc81
graciofilipe <gracio.contact@gmail.com>
2025-05-11 11:35:18 +0000

    changed the model of the text part

--- r2g_app/genai_funs.py
+++ r2g_app/genai_funs.py
@@ -7,7 +7,7 @@ from google.genai import types
 # --- Centralized Configuration Constants ---
 PROJECT_ID = os.getenv("PROJECT_ID")
 DEFAULT_VERTEX_LOCATION = "us-central1"
-PROCESS_TEXT_MODEL_NAME = "gemini-2.5-pro-preview-05-06"
+PROCESS_TEXT_MODEL_NAME = "gemini-2.5-flash-preview-04-17"
 TEXT_TO_GRAPH_MODEL_NAME = "gemini-2.5-pro-preview-05-06"
 DEFAULT_TOP_P = 1.0
 DEFAULT_MAX_TOKENS = 33333
